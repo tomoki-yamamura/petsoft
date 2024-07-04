bd67310c928cb828a3645e478e80820d792b79d5
tomoki-yamamura <progtab17@gmail.com>
2024-07-04 23:30:53 +0800

    finish dashboard page basic

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,13 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 export default function Page() {
   return (
-    <div>page</div>
+    <main>
+      <div className="flex items-center justify-between text-white py-8">
+        <Branding />
+        <Stats />
+      </div>
+    </main>
   )
 }
