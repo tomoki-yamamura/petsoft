2af5837022ca29ff0412a7fda4b9ec0eb538e573
tomoki-yamamura <progtab17@gmail.com>
2024-07-06 03:03:01 +0800

    first dialog

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,5 +1,7 @@
 import { PlusIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import PetForm from "./pet-form";
 
 type PetButtonProps = {
   actionType: "add" | "edit" | "checkout";
@@ -8,14 +10,29 @@ type PetButtonProps = {
 }
 
 export default function PetButton({ actionType, children, onClick }: PetButtonProps) {
-  if (actionType === "add") {
-    return <Button size={"icon"}>
-      <PlusIcon className="h-6 w-6" />
-    </Button>
+  if (actionType === "add" || actionType === "edit") {
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+          {actionType === "add" ? (
+            <Button size={"icon"}>
+              <PlusIcon className="h-6 w-6" />
+            </Button>
+          ) : (
+            <Button variant={"secondary"}>{children}</Button>
+          )}
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{actionType === "add" ? "Add a new pet" : "Edit pet"}</DialogTitle>
+          </DialogHeader>
+          <PetForm />
+          <DialogFooter>Footer</DialogFooter>
+        </DialogContent>
+      </Dialog>
+    )
   }
-  if (actionType === "edit") {
-    return <Button variant={"secondary"}>{children}</Button>
-  }
+
   if (actionType === "checkout") {
     return <Button variant={"secondary"} onClick={onClick}>{children}</Button>
   }
---
+++ src/components/pet-form.tsx
@@ -0,0 +1,5 @@
+export default function PetForm() {
+  return (
+    <div>Form here</div>
+  )
+}
