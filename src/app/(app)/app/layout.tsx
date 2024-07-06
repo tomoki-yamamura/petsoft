df878bad33fe74351ec0aa65e36d2b3fde03c3e4
tomoki-yamamura <progtab17@gmail.com>
2024-07-06 18:18:32 +0800

    findmany

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -3,18 +3,13 @@ import AppFooter from "@/components/app-footer"
 import AppHeader from "@/components/app-header"
 import PetContextProvider from "@/contexts/pet-context-provider"
 import SearchContextProvider from "@/contexts/search-context-provider"
-import { Pet } from "@/lib/types"
+import prisma from "@/lib/db"
 
 export default async function Layout({ children }: {
   children: React.ReactNode
 }) {
 
-  const response = await fetch("https://bytegrad.com/course-assets/projects/petsoft/api/pets")
-  if (!response.ok) {
-    throw new Error("Could not fetch pets")
-  }
-
-  const data: Pet[] = await response.json()
+  const pets = await prisma.pet.findMany()
 
   return (
     <>
@@ -22,7 +17,7 @@ export default async function Layout({ children }: {
       <div className="flex flex-col max-w-[1080px] mx-auto px-2 min-h-screen">
         <AppHeader />
         <SearchContextProvider>
-          <PetContextProvider data={data} >
+          <PetContextProvider data={pets} >
             {children}
           </PetContextProvider>
         </SearchContextProvider>
@@ -31,3 +26,4 @@ export default async function Layout({ children }: {
     </>
   )
 }
+
