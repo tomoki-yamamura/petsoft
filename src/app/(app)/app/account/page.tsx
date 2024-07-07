9210d7a84cb94217257e65c9c61e1d91ab8de6ad
tomoki-yamamura <progtab17@gmail.com>
2024-07-07 20:37:06 +0800

    next-auth

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,13 +1,20 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if(!session?.user) {
+    redirect("/login")
+  }
+
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
 
       <ContentBlock className="h-[500px] flex items-center justify-center">
-        <p>Logged in as ...</p>
+        <p>Logged in as {session.user.email}</p>
       </ContentBlock>
     </main>
   )
