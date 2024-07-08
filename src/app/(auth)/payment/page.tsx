ff949c8565ba810742b03042e18560f4b808c99d
tomoki-yamamura <progtab17@gmail.com>
2024-07-09 00:46:17 +0800

    add usetransition in payment

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -2,20 +2,26 @@
 import { createCheckoutSession } from "@/actions/actions";
 import H1 from "@/components/h1";
 import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
 
 export default function Page({ searchParams }) {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1>PetSoft access requires payment</H1>
 
       {!searchParams.success && (
         <Button
+          disabled={isPending}
           onClick={async () => {
-            await createCheckoutSession()
+            startTransition(async () => {
+              await createCheckoutSession()
+            })
           }}>Buy lifetime access for $299
         </Button>
       )}
-      
+
       {searchParams.success && (
         <p className="text-sm text-green-700">
           Payment successful! You now have lifetime access to PetSoft.
