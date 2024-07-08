e577cab2f2880225b662eb5f3aaf112b6a3bada5
tomoki-yamamura <progtab17@gmail.com>
2024-07-08 21:05:08 +0800

    payment

---
+++ src/app/(auth)/payment/page.tsx
@@ -0,0 +1,12 @@
+import H1 from "@/components/h1";
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center space-y-10">
+      <H1>PetSoft access requires payment</H1>
+
+      <Button>Buy lifetime access for $299</Button>
+    </main>
+  )
+}
