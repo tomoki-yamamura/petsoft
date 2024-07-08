c30e193ebdb0c30d31f1cf81eae7efc112344d3f
tomoki-yamamura <progtab17@gmail.com>
2024-07-09 00:27:40 +0800

    payment success display

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -3,16 +3,29 @@ import { createCheckoutSession } from "@/actions/actions";
 import H1 from "@/components/h1";
 import { Button } from "@/components/ui/button";
 
-export default function Page() {
+export default function Page({ searchParams }) {
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1>PetSoft access requires payment</H1>
 
-      <Button
-        onClick={async () => {
-          await createCheckoutSession()
-        }}>Buy lifetime access for $299
-      </Button>
+      {!searchParams.success && (
+        <Button
+          onClick={async () => {
+            await createCheckoutSession()
+          }}>Buy lifetime access for $299
+        </Button>
+      )}
+      
+      {searchParams.success && (
+        <p className="text-sm text-green-700">
+          Payment successful! You now have lifetime access to PetSoft.
+        </p>
+      )}
+      {searchParams.cancelled && (
+        <p className="text-sm text-red-700">
+          Payment cancelled. You can try again.
+        </p>
+      )}
     </main>
   )
 }
